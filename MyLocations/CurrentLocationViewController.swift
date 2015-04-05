559a20cf4ae10d5c25fb6009ee8a7ef0439d4475
Jacy Wang <jacywj@gmail.com>
2015-04-05 18:42:01 -0400

    Initialize repo

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by JIAN WANG on 4/5/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getLocation() {
+    
+    }
+
+}
+
